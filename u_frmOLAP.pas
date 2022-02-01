ef14a81bca6a24dcd9d56ce959af41acf2d8531c
tln <factorkevin@gmail.com>
2022-02-01 08:02:04 +0300

    Доработка формы u_frmOLAP, оплата-2

--- u_frmOLAP.pas
+++ u_frmOLAP.pas
@@ -37,7 +37,7 @@ type
     procedure Button2Click(Sender: TObject);
   private { Private declarations }
     procedure createDimensions(reportCd: string; setCaption: bool);
-    procedure addDimension(fieldName, fieldCaption: string; tp: Integer);
+    procedure addDimension(fieldName, fieldCaption: string; tp: Integer; setCaption: bool);
   public { Public declarations }
     procedure createOlapReport(reportCd, reportTitle, reportSigner: string);
   end;
@@ -103,137 +103,143 @@ begin
   begin
       // статистика
       // измерения
-    addDimension('mg1', 'Период', 1);
-    addDimension('reu_name', 'УК', 0);
-    addDimension('predpr_det', 'Адрес', 0);
-    addDimension('status_name', 'Статус', 0);
+    addDimension('mg1', 'Период', 1, setCaption);
+    addDimension('reu_name', 'УК', 0, setCaption);
+    addDimension('predpr_det', 'Адрес', 0, setCaption);
+    addDimension('status_name', 'Статус', 0, setCaption);
 
-    addDimension('name_usl', 'Услуга', 0);
-    addDimension('name_org', 'Организация', 0);
+    addDimension('name_usl', 'Услуга', 0, setCaption);
+    addDimension('name_org', 'Организация', 0, setCaption);
 
-    addDimension('val_group', 'Коэфф.', 1);
-    addDimension('val_group2', 'Норматив', 1);
-    addDimension('uch', 'Котельная', 1);
-    addDimension('lsk', 'Лиц.счет', 1);
-    addDimension('odpu_ex', 'ОДПУ', 1);
-    addDimension('name_gr', 'Участок', 1);
-    addDimension('psch_name', 'Признак', 1);
+    addDimension('val_group', 'Коэфф.', 1, setCaption);
+    addDimension('val_group2', 'Норматив', 1, setCaption);
+    addDimension('uch', 'Котельная', 1, setCaption);
+    addDimension('lsk', 'Лиц.счет', 1, setCaption);
+    addDimension('odpu_ex', 'ОДПУ', 1, setCaption);
+    addDimension('name_gr', 'Участок', 1, setCaption);
+    addDimension('psch_name', 'Признак', 1, setCaption);
 
-    addDimension('isHotPipe', 'Гв.изол.', 1);
-    addDimension('isTowel', 'Полот.суш', 1);
-    addDimension('fio', 'Фамилия', 1);
+    addDimension('isHotPipe', 'Гв.изол.', 1, setCaption);
+    addDimension('isTowel', 'Полот.суш', 1, setCaption);
+    addDimension('fio', 'Фамилия', 1, setCaption);
 
       // показатели
-    addDimension('cnt', 'Кол-во (Ед.)', 2);
-    addDimension('opl', 'Площадь', 2);
-    addDimension('chng_vol', 'Перерасчет', 2);
-    addDimension('klsk', 'кол-во лиц.сч.', 2);
-    addDimension('kpr', 'кол-во прожив.в т.ч.В.О.', 2);
-    addDimension('kpr_ot', 'Вр.отсут.', 2);
-    addDimension('kpr_wr', 'Вр.зарег.', 2);
-    addDimension('cnt_room', 'Комнат', 2);
-    addDimension('odpu_kub', 'ОДПУ, Ед.', 2);
-    addDimension('fact_cons', 'Факт.потреб.', 2);
-    addDimension('kr_soi', 'КР на СОИ', 2);
+    addDimension('cnt', 'Кол-во (Ед.)', 2, setCaption);
+    addDimension('opl', 'Площадь', 2, setCaption);
+    addDimension('chng_vol', 'Перерасчет', 2, setCaption);
+    addDimension('klsk', 'кол-во лиц.сч.', 2, setCaption);
+    addDimension('kpr', 'кол-во прожив.в т.ч.В.О.', 2, setCaption);
+    addDimension('kpr_ot', 'Вр.отсут.', 2, setCaption);
+    addDimension('kpr_wr', 'Вр.зарег.', 2, setCaption);
+    addDimension('cnt_room', 'Комнат', 2, setCaption);
+    addDimension('odpu_kub', 'ОДПУ, Ед.', 2, setCaption);
+    addDimension('fact_cons', 'Факт.потреб.', 2, setCaption);
+    addDimension('kr_soi', 'КР на СОИ', 2, setCaption);
   end
   else if (reportCd = '14') then
   begin
     // оборотка
       // измерения
-    addDimension('mg1', 'Период', 1);
-    addDimension('predpr', 'Фонд', 0);
-    addDimension('reu', 'УК', 0);
-    addDimension('predpr_det', 'Адрес', 0);
-    addDimension('type', 'Тип', 0);
-    addDimension('status', 'Статус', 0);
+    addDimension('mg1', 'Период', 1, setCaption);
+    addDimension('predpr', 'Фонд', 0, setCaption);
+    addDimension('reu', 'УК', 0, setCaption);
+    addDimension('predpr_det', 'Адрес', 0, setCaption);
+    addDimension('type', 'Тип', 0, setCaption);
+    addDimension('status', 'Статус', 0, setCaption);
 
-    addDimension('usl_name', 'Услуга', 0);
-    addDimension('org_name', 'Организация', 0);
-    addDimension('name_gr', 'Участок', 1);
-    addDimension('lsk', 'Лиц.счет', 1);
-    addDimension('odpu_ex', 'ОДПУ', 1);
-    addDimension('other_name', 'Прочие орг.', 1);
-    addDimension('grp', 'Группа', 1);
-    addDimension('psch_name', 'Признак', 1);
-    addDimension('isHotPipe', 'Гв.изол.', 1);
-    addDimension('isTowel', 'Полот.суш', 1);
-    addDimension('fio', 'Фамилия', 1);
+    addDimension('usl_name', 'Услуга', 0, setCaption);
+    addDimension('org_name', 'Организация', 0, setCaption);
+    addDimension('name_gr', 'Участок', 1, setCaption);
+    addDimension('lsk', 'Лиц.счет', 1, setCaption);
+    addDimension('odpu_ex', 'ОДПУ', 1, setCaption);
+    addDimension('other_name', 'Прочие орг.', 1, setCaption);
+    addDimension('grp', 'Группа', 1, setCaption);
+    addDimension('psch_name', 'Признак', 1, setCaption);
+    addDimension('isHotPipe', 'Гв.изол.', 1, setCaption);
+    addDimension('isTowel', 'Полот.суш', 1, setCaption);
+    addDimension('fio', 'Фамилия', 1, setCaption);
 
       // показатели
-    addDimension('indebet', 'Вх.дебет', 2);
-    addDimension('inkredit', 'Вх.кредит', 2);
-    addDimension('charges', 'Начислено', 2);
-    addDimension('changes', 'Скидки', 2);
-    addDimension('changes2', 'Доб./Возвр.', 2);
-    addDimension('changes3', 'Корр.сал.', 2);
-    addDimension('changeall', 'Изменения', 2);
-    addDimension('payment', 'Оплата', 2);
-    addDimension('outdebet', 'Исх.дебет', 2);
-    addDimension('outkredit', 'Исх.кредит', 2);
-    addDimension('pinsal', 'Вх.сал.пени', 2);
-    addDimension('pcur', 'Начисл.Пени', 2);
-    addDimension('pn', 'Оплач.Пени', 2);
-    addDimension('poutsal', 'Исх.сал.Пени', 2);
+    addDimension('indebet', 'Вх.дебет', 2, setCaption);
+    addDimension('inkredit', 'Вх.кредит', 2, setCaption);
+    addDimension('charges', 'Начислено', 2, setCaption);
+    addDimension('changes', 'Скидки', 2, setCaption);
+    addDimension('changes2', 'Доб./Возвр.', 2, setCaption);
+    addDimension('changes3', 'Корр.сал.', 2, setCaption);
+    addDimension('changeall', 'Изменения', 2, setCaption);
+    addDimension('payment', 'Оплата', 2, setCaption);
+    addDimension('outdebet', 'Исх.дебет', 2, setCaption);
+    addDimension('outkredit', 'Исх.кредит', 2, setCaption);
+    addDimension('pinsal', 'Вх.сал.пени', 2, setCaption);
+    addDimension('pcur', 'Начисл.Пени', 2, setCaption);
+    addDimension('pn', 'Оплач.Пени', 2, setCaption);
+    addDimension('poutsal', 'Исх.сал.Пени', 2, setCaption);
   end
   else if (reportCd = '35') then
   begin
     // оплата
       // измерения
-    addDimension('mg_name', 'Период', 1);
-    addDimension('predp', 'Фонд', 0);
-    addDimension('reu', 'УК', 0);
-    addDimension('predpr_det', 'Адрес', 0);
-    addDimension('org_name', 'Организация', 0);
-    addDimension('dat', 'Дата', 0);
-    addDimension('var_name', 'Вариант орг.', 0);
-    addDimension('dopl_name', 'Период оплаты', 0);
-    addDimension('dopl_name', 'Период оплаты', 0);
-    addDimension('nm', 'Услуга', 0);
-    addDimension('opername', 'Операция', 0);
-    addDimension('cd_tp', 'Тип поступления', 0);
+    addDimension('mg_name', 'Период', 1, setCaption);
+    addDimension('predp', 'Фонд', 0, setCaption);
+    addDimension('reu', 'УК', 0, setCaption);
+    addDimension('predpr_det', 'Адрес', 0, setCaption);
+    addDimension('org_name', 'Организация', 0, setCaption);
+    addDimension('dat', 'Дата', 0, setCaption);
+    addDimension('var_name', 'Вариант орг.', 0, setCaption);
+    addDimension('dopl_name', 'Период оплаты', 0, setCaption);
+    addDimension('dopl_name', 'Период оплаты', 0, setCaption);
+    addDimension('nm', 'Услуга', 0, setCaption);
+    addDimension('opername', 'Операция', 0, setCaption);
+    addDimension('cd_tp', 'Тип поступления', 0, setCaption);
 
       // показатели
-    addDimension('summa', 'Сумма', 2);
+    addDimension('summa', 'Сумма', 2, setCaption);
   end;
 end;
 
-procedure TfrmOLAP.addDimension(fieldName, fieldCaption: string; tp: Integer);
+procedure TfrmOLAP.addDimension(fieldName, fieldCaption: string; tp: Integer; setCaption: bool);
 var
   dim: TfcxSliceField;
   fld: TfcxSourceField;
 begin
-  with fcxDataSource1 do
+  if setCaption then
   begin
-    fld := Fields.FieldByName[fieldName];
-    if fld = nil then
+    with fcxDataSource1 do
     begin
-      Application.MessageBox(PChar('Компонент TfcxSourceField ' + fieldName + ' не найден!'), 'Внимание!', MB_OK + MB_ICONSTOP + MB_TOPMOST);
-      Exit;
-    end;
+      fld := Fields.FieldByName[fieldName];
+      if fld = nil then
+      begin
+        Application.MessageBox(PChar('Компонент TfcxSourceField ' + fieldName + ' не найден!'), 'Внимание!', MB_OK + MB_ICONSTOP + MB_TOPMOST);
+        Exit;
+      end;
 //    TfcxReferenceDataField(Fields.FieldByName[fieldName].DataField).CubeFieldDisplayLabel := fieldCaption;
-    Fields.FieldByName[fieldName].DataField.CubeFieldDisplayLabel := fieldCaption;
-  end;
-
-  with fcxSlice1 do
+      Fields.FieldByName[fieldName].DataField.CubeFieldDisplayLabel := fieldCaption;
+    end;
+  end
+  else
   begin
-    dim := SliceFieldByName[fieldName];
 
-    if tp = 0 then
+    with fcxSlice1 do
     begin
+      dim := SliceFieldByName[fieldName];
+
+      if tp = 0 then
+      begin
       // dimension
-      dim.Collapsed := true;
-      YAxisContainer.AddDimension(dim, fieldName, fieldCaption);
-    end
-    else if tp = 1 then
-    begin
+        dim.Collapsed := true;
+        YAxisContainer.AddDimension(dim, fieldName, fieldCaption);
+      end
+      else if tp = 1 then
+      begin
       // filter
-      fcxSlice1.PageContainer.AddFilterField(dim, fieldName, fieldCaption);
+        fcxSlice1.PageContainer.AddFilterField(dim, fieldName, fieldCaption);
 
-    end
-    else if tp = 2 then
-    begin
+      end
+      else if tp = 2 then
+      begin
     // measure
-      MeasuresContainer.AddMeasure(SliceFieldByName[fieldName], fieldName, fieldCaption, af_Sum);
+        MeasuresContainer.AddMeasure(SliceFieldByName[fieldName], fieldName, fieldCaption, af_Sum);
+      end;
     end;
   end;
 
